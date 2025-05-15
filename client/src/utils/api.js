321f5e25742e73045e9247c21d0d55731163c6b3
falgunyash6789 <166277998+falgunyash6789@users.noreply.github.com>
2025-05-15 15:18:51 +0530

    Delete client directory

--- client/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-  const url = process.env.URL
-const API = axios.create({
-  baseURL: `${url}/api/user`,
-});
-
-export const loginUser = (credentials) => API.post('/login', credentials);
-export const fetchAllMessages = () => API.get('/all');
-export const sendMessage = (data) => API.post('/confess', data);
